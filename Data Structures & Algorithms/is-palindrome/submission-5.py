31748da13a51cd2490974234a7337a4c22ff9888
zarch <109765046+zarchify@users.noreply.github.com>
2026-08-12 14:28:39 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,7 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.casefold()
+        filtered = ''.join([x for x in s if x.isalnum()])
+        return filtered == ''.join(reversed(filtered))
